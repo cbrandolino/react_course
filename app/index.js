6bd6fd079dd2faa76e78389f808cef8f5df0ba19
Claudio Brandolino <cbrandolino@gmail.com>
2016-06-11 10:06:37 +0200

    Nested components

--- app/index.js
+++ app/index.js
@@ -1,15 +1,54 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 
-var HelloWorld = React.createClass({
+var userData = {
+  username: 'cbrandolino',
+  imageUrl: 'http://placekitten.com/200/300',
+  name: 'Claudio Brandolino'
+}
+
+
+var ProfilePic = React.createClass({
   render: function() {
     return (
-      <div> Hello {this.props.name} </div>
-    )
+      <img src={this.props.imageUrl} />
+    );
+  }
+});
+
+var ProfileLink = React.createClass({
+  render: function() {
+    return (
+      <div>
+        <a href={'http://github.com/' + this.props.username}>
+          {this.props.username}
+        </a>
+      </div>
+    );
+  }
+});
+
+var ProfileName = React.createClass({
+  render: function() {
+    return (
+      <div className="ProfileName">{this.props.name}</div>
+    );
+  }
+});
+
+var Avatar = React.createClass({
+  render: function() {
+    return (
+      <div className="Avatar">
+        <ProfilePic imageUrl={this.props.user.imageUrl} />
+        <ProfileName name={this.props.user.name} />
+        <ProfileLink username={this.props.user.username} />
+      </div>
+    );
   }
 });
 
 ReactDOM.render(
-  <HelloWorld name="claudio" />,
+  <Avatar user={userData} />,
   document.getElementById('app')
 )
