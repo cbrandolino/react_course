240cf3a832d16cf857b3beafec414548bd787f60
Claudio Brandolino <cbrandolino@gmail.com>
2016-06-11 10:39:01 +0200

    Added router and components

---
+++ app/components/main.js
@@ -0,0 +1,14 @@
+var React = require('react');
+
+var Main = React.createClass({
+  render: function() {
+    return (
+      <div className="Main">
+        Hello From Main!
+        
+      </div>
+    );
+  }
+});
+
+module.exports = Main;
